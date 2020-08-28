987e56ff21d372508e85d42a146691b16de020ce
OoMeioO <mei791396461@foxmail.com>
2020-08-28 15:00:08 +0800

    2.28

--- mxnet_study/study_2.3.py
+++ mxnet_study/study_2.3.py
@@ -5,7 +5,37 @@
 高伟 2020.8.26
 """
 from mxnet import autograd, nd
+# #
+# x = nd.arange(4).reshape((4, 1))
+# x2 = nd.arange(4).reshape((1, 4))
+# # 申请梯度所需内存
+# x.attach_grad()
 #
-x = nd.arange(4).reshape((4, 1))
-# 申请梯度所需内存
-x.attach_grad()
+# with autograd.record():
+#     y = 2*nd.dot(x.T, x)+5*nd.dot(x.T, x)
+# y.backward()
+# # norm() L2范数
+# # x.grad就是x的梯度值
+# assert(x.grad - 4*x-10*x).norm().asscalar() == 0
+# print(x.grad)
+
+# BP算法简单实现
+x = nd.arange(8).reshape((4, 2))
+b = nd.array([[1, 1], [1, 1]])
+k = 2
+y = k*nd.dot(x.T, x) + b
+
+# 损失函数简单实现
+def loss_01():
+
+# 常用损失函数：0-1损失函数、平方损失函数、绝对损失函数、对数损失函数、交叉熵损失函数
+# 对数损失函数
+def loss_log(y_true, y_pred, ):
+    y_true = nd.array(y_true)
+    y_pred= nd.array(y_pred)
+    # 判断两者形状是否相同
+    assert(len(y_true) == len(y_pred))
+
+
+
+
