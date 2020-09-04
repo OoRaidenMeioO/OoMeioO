d1221b45ee939fdee8ac73039495b0bf9e8079a4
OoMeioO <mei791396461@foxmail.com>
2020-09-04 10:15:54 +0800

    function

--- mxnet_study/study_2.3.py
+++ mxnet_study/study_2.3.py
@@ -5,6 +5,7 @@
 高伟 2020.8.26
 """
 from mxnet import autograd, nd
+import numpy as np
 # #
 # x = nd.arange(4).reshape((4, 1))
 # x2 = nd.arange(4).reshape((1, 4))
@@ -25,16 +26,33 @@ b = nd.array([[1, 1], [1, 1]])
 k = 2
 y = k*nd.dot(x.T, x) + b
 
-# 损失函数简单实现
-def loss_01():
+# # 损失函数简单实现
+# def loss_01():
+#
+# # 常用损失函数：0-1损失函数、平方损失函数、绝对损失函数、对数损失函数、交叉熵损失函数
+# # 对数损失函数
+# def loss_log(y_true, y_pred):
+#     y_true = nd.array(y_true)
+#     y_pred= nd.array(y_pred)
+#     # 判断两者形状是否相同
+#     assert(len(y_true) == len(y_pred))
 
-# 常用损失函数：0-1损失函数、平方损失函数、绝对损失函数、对数损失函数、交叉熵损失函数
-# 对数损失函数
-def loss_log(y_true, y_pred, ):
-    y_true = nd.array(y_true)
-    y_pred= nd.array(y_pred)
-    # 判断两者形状是否相同
-    assert(len(y_true) == len(y_pred))
+# 无批处理
+# sigmoid函数,激活函数
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+# softmax函数,归一化函数
+# 重点：消除上溢和下溢，减去最大值进行运算
+def softmax(x):
+    max_x = np.max(x)
+    exp_x = np.exp(x-max_x)
+    softmax_x = exp_x/np.sum(exp_x)
+    return softmax_x
+
+def predict
+
+
 
 
 
